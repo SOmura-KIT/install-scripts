c6d158fb7a2477f720d92776eedc96c693e297cd
Shuto Omura <c1100921@planet.kanazawa-it.ac.jp>
2024-01-06 11:33:13 +0900

    ディレクトリのパスが間違ってた

--- install.sh
+++ install.sh
@@ -2,9 +2,10 @@ SCRIPT_PATH="$(realpath "$0")"
 BASE_DIR="$(dirname "$SCRIPT_PATH")"
 
 function install_neovim() {
-  if [ -d "$BASE_DIR" ]; then
+  if [ -d "$BASE_DIR/neovim" ]; then
     return
   fi
+  cd "$BASE_DIR"
   git clone https://github.com/neovim/neovim $BASE_DIR/neovim
   cd "$BASE_DIR/neovim"
   make CMAKE_BUILD_TYPE=Release CMAKE_INSTALL_PREFIX=$HOME/.local/nvim BUNDLED_CMAKE_FLAG='-DUSE_BUNDLED_TS_PARSERS=OFF' -j8
