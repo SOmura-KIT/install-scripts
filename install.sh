96e5de8ae666e9c56441ad25f5bb8b4e2293de6a
Shuto Omura <c1100921@planet.kanazawa-it.ac.jp>
2024-01-07 10:02:18 +0900

    neovimを/usr/binにリンクを貼るように変更

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ function install_neovim() {
   cd "$BASE_DIR/neovim"
   make CMAKE_BUILD_TYPE=Release CMAKE_INSTALL_PREFIX=$HOME/.local/nvim BUNDLED_CMAKE_FLAG='-DUSE_BUNDLED_TS_PARSERS=OFF' -j8
   make install
+  sudo ln -s $HOME/.local/nvim/bin/nvim /usr/bin/.
 }
 
 install_neovim
